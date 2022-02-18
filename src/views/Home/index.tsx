a4e40e4156c3d3769bcf937e2094c48febcab225
Christopher Díaz <nealthmc@gmail.com>
2022-02-17 23:12:49 -0500

    feat: Update new hero version

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Flex } from '@gravyswap/uikit'
+import { LogoWithTextIcon } from '@gravyswap/uikit'
 import PageSection from 'components/PageSection'
 import { useWeb3React } from '@web3-react/core'
 import useTheme from 'hooks/useTheme'
+import ConnectWalletButton from 'components/ConnectWalletButton'
 import Container from 'components/Layout/Container'
 import { PageMeta } from 'components/Layout/Page'
 import Hero from './components/Hero'
@@ -42,6 +43,49 @@ const UserBannerWrapper = styled(Container)`
   }
 `
 
+const HeroContainer = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 850px;
+  background: white;
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+`
+
+const HeroIconRightContainer = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+`
+
+const HeroIconLeftContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+const Logo = styled(LogoWithTextIcon)`
+  position: absolute;
+  width: 250px;
+  right: 50%;
+  transform: translateX(50%);
+  top: 50px;
+`
+
+const StyledHero = styled.div`
+  padding: 0 100px;
+  z-index: 1;
+`
+
+const StyledConnectWalletButton = styled(ConnectWalletButton)`
+  position: absolute;
+  top: 40px;
+  right: 100px;
+  z-index: 2;
+`
+
 const Home: React.FC = () => {
   const { theme } = useTheme()
   const { account } = useWeb3React()
@@ -51,7 +95,32 @@ const Home: React.FC = () => {
   return (
     <>
       <PageMeta />
-      <StyledHeroSection
+      <HeroContainer>
+        <Logo isDark={false} />
+        <StyledConnectWalletButton />
+        <HeroIconLeftContainer>
+          <svg width={700} viewBox="0 0 543 222" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0-2h543c-19.001 81.191-79.962 142.731-182.882 184.62C282.038 214.398 161.998 226.525 0 219V-2Z"
+              fill="#441F95"
+              fillRule="evenodd"
+            />
+          </svg>
+        </HeroIconLeftContainer>
+        <StyledHero>
+          <Hero />
+        </StyledHero>
+        <HeroIconRightContainer>
+          <svg width={800} viewBox="0 0 737 640" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M335.475 64.732 746-1v643L333.298 536.843C111.1 476.649 0 381.403 0 251.103c0-84.034 111.825-146.158 335.475-186.371Z"
+              fill="#FAEBC1"
+              fillRule="evenodd"
+            />
+          </svg>
+        </HeroIconRightContainer>
+      </HeroContainer>
+      {/* <StyledHeroSection
         innerProps={{ style: { margin: '0', width: '100%' } }}
         background={theme.isDark ? 'radial-gradient(103.12% 50% at 50% 50%, #21193A 0%, #191326 100%)' : '#F4D8AA'}
         index={2}
@@ -69,7 +138,7 @@ const Home: React.FC = () => {
           minHeight={150}
         />
         <Hero />
-      </StyledHeroSection>
+      </StyledHeroSection> */}
       <PageSection
         innerProps={{ style: { margin: '0', width: '100%' } }}
         background={
