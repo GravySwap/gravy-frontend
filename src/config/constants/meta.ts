bb458c83be0c9bed8e4796990363eaf5e4e19f46
Christopher Díaz <nealthmc@gmail.com>
2022-02-21 12:25:02 -0500

    fix: Update meta banner

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -5,7 +5,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'GravySwap',
   description:
     'The most popular AMM on BSC by user count! Earn GRAVY through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by GravySwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  image: 'https://gravyswap.com/images/hero.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
