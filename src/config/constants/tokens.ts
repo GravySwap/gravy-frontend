e7d00701a183dff6a8ee9d6bebe1d07432233b27
Weoka <weokap@gmail.com>
2022-03-03 02:07:15 +0100

    fix: Gravy address

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -24,7 +24,7 @@ export const mainnetTokens = {
   // bnb here points to the wbnb contract. Wherever the currency BNB is required, conditional checks for the symbol 'BNB' can be used
   bnb: new Token(MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'BNB', 'BNB', 'https://www.binance.com/'),
   gravy: new Token(
-    MAINNET,
+    TESTNET,
     '0x5eA26D0d36EAF02d63a3023521A7924e19c34454',
     18,
     'GRAVY',
@@ -32,7 +32,7 @@ export const mainnetTokens = {
     'https://gravyswap.finance/',
   ),
   cake: new Token(
-    MAINNET,
+    TESTNET,
     '0x5eA26D0d36EAF02d63a3023521A7924e19c34454',
     18,
     'GRAVY',
