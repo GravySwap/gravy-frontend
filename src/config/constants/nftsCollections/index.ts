1b7e706f364b30c4528ca86a410f5085e4c379ff
Weoka <weokap@gmail.com>
2022-03-22 15:58:17 +0100

    fix: Gravy conductors

--- src/config/constants/nftsCollections/index.ts
+++ src/config/constants/nftsCollections/index.ts
@@ -6,7 +6,7 @@ const collections: Collections = {
     slug: 'gravy-conductors',
     address: {
       56: '0x4B12229Da4Fc337A13c12050334F6821e120798A',
-      97: '0x5eA26D0d36EAF02d63a3023521A7924e19c34454',
+      97: '0xDED3E51783808AF2Eff5fCB28e71AD209402Ad58',
     },
   },
   [CollectionKey.SQUAD]: {
